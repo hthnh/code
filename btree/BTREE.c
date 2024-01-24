ad703d116733a5bed28475473ba34b0638840833
hthnh <haithinh09032006@gmail.com>
2024-01-24 19:15:18 +0700

    heh

--- btree/BTREE.c
+++ btree/BTREE.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+bool tree[10000000];
 char **S,**T;
+int diff_node = 0;
+
 // int convert(char c){
 //     if(c == 76 ) return 1;//L
 //     if(c == 82) return 2;//R
 //     if(c == 85) return 0;//U
 //     return -1;
 // }
-void input(){
+
+int input(){
     FILE *f = fopen("BTREE.INP","r");
     int temp;char temp1[100000];
     fscanf(f,"%d",&temp);
@@ -24,12 +29,41 @@ void input(){
         strcpy(T[i],temp1);
 
     }
-    for(int i = 0; i < temp; i++){
-        printf("%s\n",S[i]);
-        printf("%s\n\n",T[i]);
+    return temp;
+}
+
+int visit(char c,int root){
+    if(c == 76){
+        root *=2;
+        if(tree[root] == 0) diff_node++;
+        tree[root] = 1;
+
     }
+    if(c == 82){
+        root = root*2+1;
+        if(tree[root] == 0) diff_node++;
+        tree[root] = 1;
+    }
+    if(c == 85){
+        if(root % 2 == 0) root = root /2;
+        else root = (root -1)/2;
+    }
+    return root;
 }
 
 int main(){
-input();
+int so_cap = input();
+int root = 0;
+for(int i = 0;i<so_cap;i++){
+    for(int j = 0;j < strlen(S[i]) ; j++){
+        root = visit(S[i][j],root);
+    }
+    for(int j = 0;j < strlen(T[i]); j++){
+        root = visit(T[i][j],root);
+    }
+    printf("%d\n",diff_node);
+    diff_node = 0;
 }
+
+
+} 
