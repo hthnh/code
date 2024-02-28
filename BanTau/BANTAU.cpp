13ce11615ca7159bc9ea41a94148c448f6f1bd70
hthnh <haithinh09032006@gmail.com>
2024-02-28 09:07:25 +0700

    aa

--- BanTau/BANTAU.cpp
+++ BanTau/BANTAU.cpp
@@ -2,8 +2,8 @@
 #include <stdbool.h>
 #define max 450
 int num_of_er; // energy recharge
-int num_of_ship;
-int di[max],dt[max], point[max];
+int num_of_ship,h = 0;
+int di[max],dt[max], point[max],result,pro[2][max];
 
 void input(){
     FILE *f = fopen("BANTAU.INP","r");
@@ -19,32 +19,60 @@ void output(){
     FILE *f = fopen("BANTAU.OUT","w");
     fclose(f);
 }
-void aa(int start, int end){
-    int x = start;
-    int temp;
-    for(int y = start; y < end; y++){
-        temp = di[y];
-        for(int i = y; i >= 0; i--){
-            if(temp < di[i]){
-                temp = di[y];
-                break;
-            }
-            if(temp >= di[i]) point[i] = temp - di[i];
-        }
-        if(start == end) return;
-        aa(start +1,end);
+
+void dupl(){
+    for(int i = 0; i < num_of_ship; i++){
+        dt[i] = di[i];
     }
-    return;
+}
+
+void sort(int *p, int num){
+    int temp;
+    for(int i=0; i<num; i++){
+        for(int j=num-1; j>i; j--){
+            if(p[j] > p[j-1]){
+                temp = p[j];
+                p[j] = p[j-1];
+                p[j-1] = temp;
+            }
+        }
+    }
+}
+
+
+int aa(int sum, int *atk, int *hp, int turn){
+    if(!(turn < num_of_ship)){
+        turn--;
+        if(sum > result) result = sum;
+        return sum;
+    }
+    for(int i = 0; i < num_of_ship; i ++){
+        if(atk[i] >= hp[turn]){ 
+            sum += atk[i] - hp[turn];
+            pro[0][h] = hp[turn];
+            pro[1][h] = atk[i];
+            h++;
+            turn++;
+            sum = aa(sum, atk, hp, turn);
+        }
+        if(atk[i] < hp[turn]){
+            h = 0;
+            continue;
+        }
+    }
+    return 0;
 }
 
 
 int main(){
 
     input();
-    int start = 0, end = num_of_ship;
+    dupl();
+    sort(dt,num_of_ship);
 
-    aa(start,end);
+    point[0] = aa(0, dt, di, 0);
+
     for(int i = 0; i < num_of_ship; i++){
-        printf("%d ",point[i]);
+        printf("%d ",dt[i]);
     }
 }
