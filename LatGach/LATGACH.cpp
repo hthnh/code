3381355717869cfb7de09988c558a1e2f9ea7796
hthnh <haithinh09032006@gmail.com>
2024-02-28 16:46:54 +0700

    f

--- LatGach/LATGACH.cpp
+++ LatGach/LATGACH.cpp
@@ -10,8 +10,10 @@ struct Gach{
     local p3;
     local p4;
     int acreage;
+    int id_link;
 };
 struct Gach *G;
+int current_link = 1;
 
 int input(){
     int so_gach;
@@ -27,12 +29,13 @@ int input(){
         G[i].p2.x = X;
         G[i].p2.y = Y+C;
         
-        G[i].p3.x = X+D;
-        G[i].p3.y = Y;
-
         G[i].p4.x = X+D;
-        G[i].p4.y = Y+C;
+        G[i].p4.y = Y;
+
+        G[i].p3.x = X+D;
+        G[i].p3.y = Y+C;
         G[i].acreage = C*D;
+        G[i].id_link = 0;
     }
     return so_gach;
 }
@@ -40,10 +43,39 @@ int input(){
 
 
 int main(){
-    // for(int i = 0; i < input();i++ ){
-    //     printf("%d %d\n%d %d\n%d %d\n%d %d \n\n",G[i].p1.x, G[i].p1.y, G[i].p2.x, G[i].p2.y, G[i].p3.x, G[i].p3.y, G[i].p4.x, G[i].p4.y );
-    // }
-    for(int i = 0;)
+    int num = input();
+    
+    for(int i = 0; i < num; i++ ){
+        if(G[i].id_link == 0){
+            G[i].id_link = current_link;
+            current_link++;
+        }     
+        for(int j = 0; j < num; j++){
+            if(i == j) continue;
+            if((G[i].p1.x == G[j].p3.x || G[i].p1.x == G[j].p4.x) && (G[i].p1.y <= G[j].p3.y || G[i].p2.y >= G[j].p3.y)) G[j].id_link = G[i].id_link;
 
+            if((G[i].p2.x == G[j].p3.x || G[i].p2.x == G[j].p4.x) && (G[i].p1.y <= G[j].p4.y || G[i].p2.y >= G[j].p4.y)) G[j].id_link = G[i].id_link;
+
+
+            if((G[i].p1.x == G[j].p3.x || G[i].p1.x == G[j].p2.x) && (G[i].p1.x <= G[j].p2.x || G[i].p4.x >= G[j].p2.x)) G[j].id_link = G[i].id_link;
+
+            if((G[i].p4.x == G[j].p3.x || G[i].p4.x == G[j].p2.x) && (G[i].p1.x <= G[j].p3.x || G[i].p4.x >= G[j].p3.x)) G[j].id_link = G[i].id_link;
+
+
+            if((G[i].p2.x == G[j].p1.x || G[i].p2.x == G[j].p4.x) && (G[i].p1.x <= G[j].p2.x || G[i].p4.x >= G[j].p2.x)) G[j].id_link = G[i].id_link;
+
+            if((G[i].p3.x == G[j].p1.x || G[i].p3.x == G[j].p4.x) && (G[i].p1.x <= G[j].p3.x || G[i].p4.x >= G[j].p3.x)) G[j].id_link = G[i].id_link;
+
+
+            if((G[i].p3.x == G[j].p2.x || G[i].p4.x == G[j].p2.x) && (G[i].p3.y >= G[j].p2.y || G[i].p4.y <= G[j].p2.y)) G[j].id_link = G[i].id_link;
+
+            if((G[i].p3.x == G[j].p1.x || G[i].p4.x == G[j].p1.x) && (G[i].p3.y >= G[j].p1.y || G[i].p4.y <= G[j].p1.y)) G[j].id_link = G[i].id_link;
+
+        }
+    }
+
+    for(int i = 0; i < num;i++ ){
+        printf("%d %d\n%d %d\n%d %d\n%d %d\n%d   %d\n\n",G[i].p1.x, G[i].p1.y, G[i].p2.x, G[i].p2.y, G[i].p3.x, G[i].p3.y, G[i].p4.x, G[i].p4.y,G[i].acreage,G[i].id_link );
+    }
     return 0;
 }
