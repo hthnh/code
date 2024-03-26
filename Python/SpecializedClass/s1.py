7a413d000f238962a7a407c83df098ce562f7110
hthnh <haithinh09032006@gmail.com>
2024-03-26 19:08:17 +0700

    dd

--- Python/SpecializedClass/s1.py
+++ Python/SpecializedClass/s1.py
@@ -1,14 +1,6 @@
-so1 = int(input('nhap gia tri cua a= '))
-so2 = int(input('nhap gia tri cua b= '))
-
-tong = so1 + so2
-hieu  = so1 - so2
-tich = so1 * so2
-thuong = so1 / so2
-
-print(tong)
-print(hieu)
-print(tich)
-print(thuong)
-
-end = input()
+import random
+with open('data.txt', 'w') as f:
+    f.write("1000")
+    for i in range(1,1001):
+        f.write("\n")
+        f.write(str(random.randint(1,10000)))
